f03a8683327f91c52804677409c15b40272a32a0
Jziumo <zijin2020@qq.com>
2024-11-14 12:00:47 -0800

    display to Howard Bro, cum in!

--- problem_a.py
+++ problem_a.py
@@ -3,8 +3,9 @@ import read_data
 
 def main():
     # @注意：可以尝试添加第2个参数，表示只计算前n个订单的结果 
-    # data = read_data.readData('a', 40)
-    data = read_data.readData('a')
+    data = read_data.readSortedData('a', sort_by='weight', ascending=True, first_n_rows=None)
+    # data = read_data.readSortedData('a', sort_by='weight', ascending=True, first_n_rows=40)
+    # data = read_data.readData('a')
 
     size = len(data['order number'])
     # print(size)
@@ -46,12 +47,30 @@ def main():
         
     # Objective: minimize the number of containers used.
     solver.Minimize(solver.Sum([y[j] for j in range(size)]))
+    
+    # start solving
     print(f"Solving with {solver.SolverVersion()}")
+    with open('./output/result_a.txt', "w") as file: 
+        file.write(f"Solving with {solver.SolverVersion()}\n")
     status = solver.Solve()
 
+    # result string
+    output_message = ""
+
     if status == pywraplp.Solver.OPTIMAL:
-        output_message = ""
+        # if the problem is solved
+        # print the result and analyze the solutions
+        
         num_containers = 0
+
+        # variables used to calculate the utilization rate
+        weights = []
+        volumes = []
+        pallets = []
+        rates = []
+
+        # check if all the containers satisfy the capacity constraint
+        all_satisfied = True
 
         # iterate each container
         for j in range(size): 
@@ -70,31 +89,68 @@ def main():
                         container_volume += data['volume'][i]
                         container_pallets += data['pallets'][i]
 
+                # record the weightss, volumes, and pallets
+                weights.append(container_weight)
+                volumes.append(container_volume)
+                pallets.append(container_pallets)
+
                 output_message += f"Container {j}:\n" 
+                output_message += f"- Orders packed: {container_items}\n"
+                output_message += f"- Weight: {container_weight}\n"
+                output_message += f"- Volume: {container_volume}\n"
+                output_message += f"- Pallets: {container_pallets}\n"
                 
                 # check if the container satisfies the capacity constraint
+                satisfy_contraint = True
                 if container_weight <= max_weight and container_volume <= max_volume and container_pallets <= max_pallets:
-
-                    output_message += f"- Orders packed: {container_items}\n"
-                    output_message += f"- Weight: {container_weight}\n"
-                    output_message += f"- Volume: {container_volume}\n"
-                    output_message += f"- Pallets: {container_pallets}\n"
                     output_message += "(satisfies the capacity constraints)\n"
-
                 else: 
                     output_message += "container is overloaded!\n"
+                    satisfy_contraint = False
+                    all_satisfied = False
+
+                # if satisfy the constraint, then analyze the utilization rate
+                if satisfy_contraint:
+                    utilization_rate = (container_weight / max_weight + container_volume / max_volume + container_pallets / max_pallets) / 3
+                    rates.append(utilization_rate)
+                    output_message += f"Average utilization rate of the container: {utilization_rate}\n"
 
                 output_message += "\n"
-
                 num_containers += 1
-    
+
+        output_message += "Total analysis: \n"
+
+        # calculate the total amount 
+        total_weight = sum(weights)
+        total_volume = sum(volumes)
+        total_pallets = sum(pallets)
+        
+        # calculate the utilization rate of each column
+        weight_rate = total_weight / (max_weight * num_containers)
+        volume_rate = total_volume / (max_volume * num_containers)
+        pallets_rate = total_pallets / (max_pallets * num_containers)
+
+        output_message += "|- Utilization rate"
+        # 写到一半你就要看代码😡 
+
+
         output_message += f"Total number of containers used: {num_containers}\n"
+
+        # output if all the containers satisfy the capacity constraint
+        if all_satisfied: 
+            output_message += "All containers satisfy the capacity constraints.\n"
+        else: 
+            output_message += "Some containers are overloaded!\n"
 
         print(output_message) 
 
-        # write the result to the file
-        with open("output/result_a.txt", "w") as file: 
-            file.write(output_message)           
+    else: 
+        # the problem is not solved for some reason
+        output_message += f"The problem is not solved. The status is {status}\n"
+
+    # write the result to the file
+    with open("output/result_a.txt", "a") as file: 
+        file.write(output_message)    
 
 if __name__=="__main__":
     main()
--- read_data.py
+++ read_data.py
@@ -3,17 +3,17 @@ import pandas as pd
 data_1a_path = "./data/Term project data 1a.csv"
 data_1b_path = "./data/Term project data 1b.csv"
 
-def readData(task, first_n_rows = None) :
+# column names mapping to the original csv file
+col_names = {"order number": "Order Number", "weight": "Weight (lbs)", "volume": "Volume (in3)", "pallets": "Pallets"}
+
+def readData(task, first_n_rows = None):
     file_path = ''
     if task == 'a':
         file_path = data_1a_path
     elif task == 'b':
         file_path = data_1b_path
 
-    data = pd.read_csv(file_path, usecols=[0, 1, 2, 3])
-
-    # drop the missing values
-    data = data.dropna(subset=[data.columns[0]])
+    data = read(file_path)
 
     # get the first n rows if specified
     if first_n_rows is not None:
@@ -21,13 +21,59 @@ def readData(task, first_n_rows = None) :
 
     print("The rows and columns of the data:", data.shape[0], 'x', data.shape[1])
 
+    map = getMap(data)
+
+    return map
+
+# get sorted data
+def readSortedData(task, sort_by='weight', ascending=True, first_n_rows = None): 
+    file_path = ''
+    if task == 'a':
+        file_path = data_1a_path
+    elif task == 'b':
+        file_path = data_1b_path
+
+    data = read(file_path)
+    
+    # sort the data frame
+    data = sort(data, sort_by=sort_by, ascending=ascending)
+
+    # get the first n rows if specified
+    if first_n_rows is not None:
+        data = data.head(first_n_rows)
+
+    print("The rows and columns of the data:", data.shape[0], 'x', data.shape[1])
+
+    map = getMap(data)
+
+    return map
+
+# Get the data frame of the .csv file
+def read(file_path):
+    data = pd.read_csv(file_path, usecols=[0, 1, 2, 3])
+    # drop the missing values
+    data = data.dropna(subset=[data.columns[0]])
+    return data
+
+# convert the data frame to a python list (map)
+def getMap(data):
     map = {}
     map['order number'] = [str(int(order)) for order in data['Order Number'].tolist()]
     map['weight'] = [int(weight) for weight in data['Weight (lbs)'].tolist()]
     map['volume'] = [int(volume) for volume in data['Volume (in3)'].tolist()]
     map['pallets'] = [int(pallets) for pallets in data['Pallets'].tolist()]
-
     return map
+
+# sort the data frame and return
+def sort(data, sort_by='weight', ascending=True): 
+    sort_by = col_names[sort_by]
+    data.sort_values(by=sort_by, ascending=ascending, inplace=True)
+    return data
+
+# shuffle the data frame and return
+def shuffle(data): 
+    return data.sample(frac=1)
+
 
 
 readData('a')
